f7b94fc1f7fe61ebb0dac2ad61b31f4d253f63b6
Thomas Couderc <thomas.couderc@gmail.com>
2015-01-23 16:50:03 +0100

    remove old tabs and add photos and gallery

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
+Used to build project
 $ npm install -g cordova ionic
 $ ionic start mystore-ionic blank
 $ cd mystore-ionic
 $ ionic platform add android
 $ ionic build android
+
+
+$ ionic serve
+$ ripple emulate
 $ ionic emulate android
 $ ionic run android
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, Camera) {
+.controller('PhotosCtrl', function($scope, Camera) {
   $scope.getPhoto = function() {
     console.log('Getting camera');
     Camera.getPicture().then(function(imageURI) {
@@ -17,45 +17,43 @@ angular.module('starter.controllers', [])
   }
 })
 
+.controller('GalleryCtrl', function($scope) {
+  $scope.noMoreItemsAvailable = false;
 
-.controller('PhotosCtrl', function($scope, /*Friends,*/ Camera) {
-  $scope.getPhoto = function() {
-    console.log('Getting camera');
-    Camera.getPicture().then(function(imageURI) {
-      console.log(imageURI);
-      $scope.lastPhoto = imageURI;
-    }, function(err) {
-      console.err(err);
-    }, {
-      quality: 75,
-      targetWidth: 320,
-      targetHeight: 320,
-      saveToPhotoAlbum: false
-    });
-  }
-})
+  $scope.loadMore = function() {
+    $scope.items.push({ id: $scope.items.length});
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
+    if ( $scope.items.length == 99 ) {
+      $scope.noMoreItemsAvailable = true;
+    }
+    $scope.$broadcast('scroll.infiniteScrollComplete');
   };
+
+  $scope.items = [];
+
 });
+
+//.controller('ChatsCtrl', function($scope, Chats) {
+//  $scope.chats = Chats.all();
+//  $scope.remove = function(chat) {
+//    Chats.remove(chat);
+//  }
+//})
+//
+//.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+//  $scope.chat = Chats.get($stateParams.chatId);
+//})
+//
+//.controller('FriendsCtrl', function($scope, Friends) {
+//  $scope.friends = Friends.all();
+//})
+//
+//.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+//  $scope.friend = Friends.get($stateParams.friendId);
+//})
+//
+//.controller('AccountCtrl', function($scope) {
+//  $scope.settings = {
+//    enableFriends: true
+//  };
+//});
